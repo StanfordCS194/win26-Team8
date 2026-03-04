065c10341de9da9e02161709eb702831f22bbc08
minamky <minaky@stanford.edu>
2026-03-04 15:01:15 -0800

    Deleting extra db files

--- mindful/database/migrations/add_product_url.sql
+++
@@ -1,5 +0,0 @@
--- Add product_url to items for duplicate-URL detection when adding to cart
-ALTER TABLE public.items
-ADD COLUMN IF NOT EXISTS product_url TEXT;
-
-COMMENT ON COLUMN public.items.product_url IS 'Product/page URL when item was added; used to detect duplicates.';
